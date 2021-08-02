330174b62297046e6b70a453a8d537112603457d
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-08-02 13:02:07 +0100

    Initial commit

---
+++ SkillUpgrades/SkillUpgrades.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Modding;
+
+namespace SkillUpgrades
+{
+    public class SkillUpgrades : Mod
+    {
+        internal static SkillUpgrades instance;
+
+        public override void Initialize()
+        {
+            instance = this;
+            instance.Log("Initializing");
+
+
+        }
+
+        public override string GetVersion()
+        {
+            return "0.1";
+        }
+
+    }
+}
