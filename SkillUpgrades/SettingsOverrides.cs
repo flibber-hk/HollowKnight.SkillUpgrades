b92b557f16991e07be7e78d2a36c0fe840b95e16
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-10-10 11:59:32 +0100

    Add Getters

--- SkillUpgrades/SettingsOverrides.cs
+++ SkillUpgrades/SettingsOverrides.cs
@@ -10,6 +10,57 @@ namespace SkillUpgrades
         internal static Dictionary<string, bool> SkillLoadOverrides { get; set; } = new Dictionary<string, bool>();
         public static bool AlreadyLoadedSkills { get; internal set; } = false;
 
+        #region Skill Getters
+        /// <summary>
+        /// Get the value of a skill override
+        /// </summary>
+        /// <param name="skillName">The name of the skill</param>
+        /// <returns>True if enabled, false if disabled, null if no override</returns>
+        public static bool? GetSkill(string skillName)
+        {
+            if (SkillUpgrades.localOverrides.EnabledSkills.TryGetValue(skillName, out bool localState)) return localState;
+            else return null;
+        }
+
+        /// <summary>
+        /// Get the value of an int field override
+        /// </summary>
+        /// <param name="skillName">The name of the skill</param>
+        /// <param name="intName">The name of the int</param>
+        /// <returns>The value of the int field, or null if no override</returns>
+        public static int? GetInt(string skillName, string intName)
+        {
+            string key = SkillUpgradeSettings.GetKey(skillName, intName);
+            if (SkillUpgrades.localOverrides.Integers.TryGetValue(key, out int value)) return value;
+            return null;
+        }
+        /// <summary>
+        /// Get the value of a bool field override
+        /// </summary>
+        /// <param name="skillName">The name of the skill</param>
+        /// <param name="boolName">The name of the bool</param>
+        /// <returns>The value of the bool field, or null if no override</returns>
+        public static bool? GetBool(string skillName, string boolName)
+        {
+            string key = SkillUpgradeSettings.GetKey(skillName, boolName);
+            if (SkillUpgrades.localOverrides.Booleans.TryGetValue(key, out bool value)) return value;
+            return null;
+        }
+        /// <summary>
+        /// Get the value of a float field override
+        /// </summary>
+        /// <param name="skillName">The name of the skill</param>
+        /// <param name="floatName">The name of the float</param>
+        /// <returns>The value of the float field, or null if no override</returns>
+        public static float? GetFloat(string skillName, string floatName)
+        {
+            string key = SkillUpgradeSettings.GetKey(skillName, floatName);
+            if (SkillUpgrades.localOverrides.Floats.TryGetValue(key, out float value)) return value;
+            return null;
+        }
+        #endregion
+
+        #region Skill Setters
         /// <summary>
         /// Set the value of whether a skill is active.
         /// </summary>
@@ -50,7 +101,7 @@ namespace SkillUpgrades
         {
             SkillUpgrades.localOverrides.SetFloat(skillName, floatName, set);
         }
-
+        #endregion
 
         /// <summary>
         /// Force a skill to initialize, or not to. Must be run before SkillUpgrades is initialized.
