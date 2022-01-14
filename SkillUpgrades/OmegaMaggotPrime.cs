98c7dccffd0ae6ac35cd3339e4b6903c3d1185e9
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-14 10:21:03 +0000

    fix bug while I'm waiting for 56 to merge

---
+++ SkillUpgrades/OmegaMaggotPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillUpgrades
+{
+    internal static class OmegaMaggotPrime
+    {
+        internal static IEnumerable<Type> _GetTypesSafely(this Assembly asm)
+        {
+            try
+            {
+                return asm.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                foreach (Type type in ex.Types.Where(x => x is not null))
+                {
+                    SkillUpgrades.instance.Log(type.FullName);
+                }
+                return ex.Types.Where(x => x is not null);
+            }
+        }
+    }
+}
