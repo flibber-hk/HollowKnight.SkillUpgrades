e3baed19705866308ca699842b54088d3313daaf
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-14 10:21:03 +0000

    Correctly use GetTypesSafely everywhere

--- SkillUpgrades/SkillUpgrades.cs
+++ SkillUpgrades/SkillUpgrades.cs
@@ -34,7 +34,7 @@ namespace SkillUpgrades
             instance.Log("Initializing");
             DebugMod.AddActionToKeyBindList(() => { ApplyGlobalToggle(!GS.GlobalToggle); RefreshMainMenu(); }, "Global Toggle", "SkillUpgrades");
 
-            foreach (Type t in Assembly.GetAssembly(typeof(SkillUpgrades)).GetTypes().Where(x => x.IsSubclassOf(typeof(AbstractSkillUpgrade))))
+            foreach (Type t in typeof(SkillUpgrades).Assembly.GetTypesSafely().Where(x => x.IsSubclassOf(typeof(AbstractSkillUpgrade))))
             {
                 AbstractSkillUpgrade skill = (AbstractSkillUpgrade)Activator.CreateInstance(t);
                 _skills.Add(skill.Name, skill);
