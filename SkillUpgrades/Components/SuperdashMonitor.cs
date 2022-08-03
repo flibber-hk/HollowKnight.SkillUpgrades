7839749ce4b794e497fb907652b75f873f807697
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-08-03 17:40:31 +0100

    Attempt to work with Transcendence

--- SkillUpgrades/Components/SuperdashMonitor.cs
+++ SkillUpgrades/Components/SuperdashMonitor.cs
@@ -7,22 +7,80 @@ namespace SkillUpgrades.Components
     /// </summary>
     public class SuperdashMonitor : MonoBehaviour
     {
+        private enum OverrideState
+        {
+            /// <summary>
+            /// Collider is disabled
+            /// </summary>
+            Disabled,
+            /// <summary>
+            /// Collider should be disabled, but we are overriding it
+            /// </summary>
+            Overridden,
+            /// <summary>
+            /// Collider is enabled
+            /// </summary>
+            Enabled
+        }
+
         private Collider2D _col;
+        private OverrideState ColliderOverrideState;
 
         void Start()
         {
             _col = GetComponent<Collider2D>();
+            ColliderOverrideState = _col.enabled ? OverrideState.Enabled : OverrideState.Disabled;
         }
 
         void Update()
         {
-            if (!_col.enabled && HeroController.instance.cState.superDashing)
+            switch (ColliderOverrideState)
             {
-                _col.enabled = true;
-            }
-            else if (_col.enabled && !HeroController.instance.cState.superDashing)
-            {
-                _col.enabled = false;
+                case OverrideState.Disabled:
+                    if (_col.enabled)
+                    {
+                        // Lost track of collider state
+                        ColliderOverrideState = OverrideState.Enabled;
+                        break;
+                    }
+                    if (HeroController.instance.cState.superDashing)
+                    {
+                        // Knight is superdashing, we should override to make the collider active
+                        _col.enabled = true;
+                        ColliderOverrideState = OverrideState.Overridden;
+                    }
+                    break;
+                case OverrideState.Enabled:
+                    if (!_col.enabled)
+                    {
+                        // Lost track of collider state
+                        ColliderOverrideState = OverrideState.Disabled;
+                    }
+                    // No need to toggle the collider - something else wants it active, and we don't *want* it inactive
+                    break;
+                case OverrideState.Overridden:
+                    if (!_col.enabled)
+                    {
+                        // Lost track of collider state
+                        if (HeroController.instance.cState.superDashing)
+                        {
+                            // Reactivate collider
+                            _col.enabled = true;
+                        }
+                        else
+                        {
+                            // Resynchronize state
+                            ColliderOverrideState = OverrideState.Disabled;
+                        }
+                        break;
+                    }
+                    if (!HeroController.instance.cState.superDashing)
+                    {
+                        // Knight is not superdashing; we should stop overriding
+                        _col.enabled = false;
+                        ColliderOverrideState = OverrideState.Disabled;
+                    }
+                    break;
             }
         }
     }
