33c9e8cd7285376c9679b10fe4863420d9297223
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-11-18 06:52:02 +0000

    Keep old fireball spawn actions available (#4)

--- SkillUpgrades/Util/FsmExtensions.cs
+++ SkillUpgrades/Util/FsmExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using HutongGames.PlayMaker;
 using HutongGames.PlayMaker.Actions;
 
@@ -34,6 +35,29 @@ namespace SkillUpgrades.Util
             fsm.FsmVariables.FloatVariables = floatVariables;
 
             return newFsmFloat;
+        }
+
+        public static void RedirectTransitionTo(this FsmState state, string origTarget, string newTarget)
+        {
+            FsmState newTargetState = state.Fsm.GetState(newTarget);
+            foreach (FsmTransition trans in state.Transitions.Where(t => t.ToFsmState?.Name == origTarget))
+            {
+                SkillUpgrades.instance.LogDebug($"RTT: {state.Name} -> {origTarget} to {newTarget}");
+                trans.ToFsmState = newTargetState;
+                trans.ToState = newTarget;
+            }
+        }
+
+        /// <summary>
+        /// Make all transitions to origTarget instead point to newTarget
+        /// Doesn't work with global transitions pointing to origTarget
+        /// </summary>
+        public static void RedirectAllTransitionsTo(this PlayMakerFSM fsm, string origTarget, string newTarget)
+        {
+            foreach (FsmState state in fsm.FsmStates)
+            {
+                state.RedirectTransitionTo(origTarget, newTarget);
+            }
         }
     }
 }
